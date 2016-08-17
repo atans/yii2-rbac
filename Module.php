dd63f9b1ed97160f3c82514239de83bf9da2c341
Atans Chiu <atans.chiu@gmail.com>
2016-08-17 15:49:56 +0800

    Update boostrap

--- Module.php
+++ Module.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace atans\rbac;
+use yii\base\BootstrapInterface;
 
 /**
  * rbac module definition class
  */
-class Module extends \yii\base\Module
+class Module extends \yii\base\Module implements BootstrapInterface
 {
     /**
      * @inheritdoc
@@ -13,6 +14,20 @@ class Module extends \yii\base\Module
     public $controllerNamespace = 'atans\rbac\controllers';
 
     public $itemNamePattern = '/^[\w][\w-.:]+[\w]$/';
+
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app)
+    {
+        if (! isset($app->get('i18n')->translations['rbac*'])) {
+            $app->get('i18n')->translations['rbac*'] = [
+                'class'    => 'yii\i18n\PhpMessageSource',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
+
 
     /**
      * @inheritdoc
